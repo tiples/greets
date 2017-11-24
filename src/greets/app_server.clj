edccf298fff62c66ca897f5d6bac876163421b69
Bill La Forge <laforge49@gmail.com>
2017-11-24 11:43:49 -0500

    added metadata to filename

--- src/greets/app_server.clj
+++ src/greets/app_server.clj
@@ -1,7 +1,8 @@
 (ns greets.app-server
     (:require
       [greets.sente-server :as sente-server]
-      [greets.atoms :as atoms]))
+      [greets.atoms :as atoms]
+      [greets.files :as files]))
 
 
 (defn -main "For `lein run`, etc." []
