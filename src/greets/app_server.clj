85bb4c2f9f1f7580223dcfa665af51d91015b22c
Bill La Forge <laforge49@gmail.com>
2017-11-24 12:05:37 -0500

    load accounts file

--- src/greets/app_server.clj
+++ src/greets/app_server.clj
@@ -1,9 +1,17 @@
 (ns greets.app-server
     (:require
+      [clojure.edn :as edn]
       [greets.sente-server :as sente-server]
       [greets.atoms :as atoms]
       [greets.files :as files]))
 
 
-(defn -main "For `lein run`, etc." []
+(defn -main "For `lein run`, etc."
+      []
+      (let [accounts-filename-map (files/resolve-file "data" "accounts" nil "edn")
+            accounts-filename (files/file-str accounts-filename-map)
+            _ (println "Loading file" accounts-filename)
+            accounts (slurp accounts-filename)
+            accounts-map (edn/read-string accounts)]
+           (println accounts-map))
       (sente-server/start! 3001))
