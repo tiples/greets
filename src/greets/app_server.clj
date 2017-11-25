9d3dcea549b341c3ffb8fdbf68c249da6b432971
Bill La Forge <laforge49@gmail.com>
2017-11-25 00:21:57 -0500

    token param becomes value of hidden element

--- src/greets/app_server.clj
+++ src/greets/app_server.clj
@@ -1,13 +1,30 @@
 (ns greets.app-server
-    (:require
-      [clojure.edn :as edn]
-      [greets.sente-server :as sente-server]
-      [greets.atoms :as atoms]
-      [greets.files :as files]))
+  (:require
+    [clojure.edn :as edn]
+    [hiccup.core :as hiccup]
+    [greets.sente-server :as sente-server]
+    [greets.atoms :as atoms]
+    [greets.files :as files]))
 
+(defn landing-pg-handler
+  [ring-req]
+  (let [params (:params ring-req)
+        token (:token params)]
+    (println token)
+    (hiccup/html
+      [:head
+       [:meta {:charset "utf-8"}]
+       [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+       [:meta {:name "description" :content ""}]
+       [:title "greets"]]
+      [:body
+       [:input {:type "hidden" :id "fudge" :name "foo" :value token}]
+       [:div {:id "container"}]
+       [:script {:type "text/javascript" :src "app.js"}]])))
 
 (defn -main "For `lein run`, etc."
-      []
-      (files/load-edn-file (files/resolve-file "data" "accounts" nil "edn") atoms/accounts)
-      (println @atoms/accounts)
-      (sente-server/start! 3001))
+  []
+  (reset! atoms/app-handler landing-pg-handler)
+  (files/load-edn-file (files/resolve-file "data" "accounts" nil "edn") atoms/accounts)
+  (println @atoms/accounts)
+  (sente-server/start! 3001))
--- src/greets/atoms.clj
+++ src/greets/atoms.clj
@@ -1,3 +1,4 @@
 (ns greets.atoms)
 
 (def accounts (atom nil))
+(def app-handler (atom nil))
