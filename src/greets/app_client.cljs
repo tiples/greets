dab0045b62b67578ad1aa5da23b642f20535fc55
Bill La Forge <laforge49@gmail.com>
2017-11-26 10:38:29 -0500

    stubbed get and got login token

--- src/greets/app_client.cljs
+++ src/greets/app_client.cljs
@@ -1,18 +1,28 @@
 (ns greets.app-client
   (:require [greets.sente-client :as client :refer [->output! chsk-send! chsk chsk-state]]
+            [greets.atoms-client :as atoms]
             [clojure.string :as str]
             [reagent.core :as reagent :refer [atom]]
             [taoensso.sente :as sente :refer (cb-success?)]))
 
-(defn calling-component
+(defn get-login-token
   []
   (let [token-status-message-element (.getElementById js/document "tokenStatusMessage")
-        token-status-message (.getAttribute token-status-message-element "value")
-        token-element (.getElementById js/document "token")
-        token (.getAttribute token-element "value")]
+        token-status-message (.getAttribute token-status-message-element "value")]
     [:div
-     (if (some? token-status-message) [:p token-status-message])
-     ]))
+     [:p token-status-message]]))
+
+(defn got-login-token
+  [token]
+  [:p (str "token=" token)])
+
+(defn calling-component
+  []
+  (let [token-element (.getElementById js/document "token")
+        token (.getAttribute token-element "value")]
+    (if (some? token)
+      [got-login-token token]
+      [get-login-token])))
 
 (defn start!
   []
