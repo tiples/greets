cb6421c48731b3a5b40696f1430e812d2f193846
Bill La Forge <laforge49@gmail.com>
2017-11-30 11:14:53 -0500

    db draft

--- src/greets/db.clj
+++ src/greets/db.clj
@@ -12,14 +12,14 @@
   [db journal-entry]
   (throw (Exception. (str "Unrecognized Journal-entry-id: " (:journal-entry-id journal-entry)))))
 
-(defn initialize
+(defn initialize!
   [db-atom folder base label value]
   (reset! db-atom {:folder folder
                    :base   base
                    :label  label
                    :value  value}))
 
-(defn close-journal
+(defn close-journal!
   [db-atom]
   (swap! db-atom
          (fn [db]
@@ -40,17 +40,17 @@
         file-map (files/file-map folder file-prefix base label suffix "edn")]
     (files/file-str file-map)))
 
-(defn write-ledger
+(defn write-ledger!
   [db-atom]
   (let [ledger-file-name (build-file-name db-atom "ledger")
         value (:value @db-atom)
         edn-value (pr-str value)]
-    (close-journal db-atom)
+    (close-journal! db-atom)
     (io/make-parents ledger-file-name)
     (with-open [ledge-writer (io/writer ledger-file-name :encoding "UTF-8")]
       (.write ledge-writer edn-value))))
 
-(defn open-journal
+(defn open-journal!
   [db-atom]
   (let [journal-writer (:journal-writer @db-atom)]
     (if (some? journal-writer)
@@ -61,18 +61,18 @@
         (swap! db-atom assoc :journal-writer journal-writer)
         journal-writer))))
 
-(defn post-journal-entry
+(defn post-journal-entry!
   [db-atom journal-entry]
   (swap! db-atom journal-entry-handler journal-entry))
 
-(defn write-journal-entry
+(defn write-journal-entry!
   [db-atom journal-entry]
-  (post-journal-entry db-atom journal-entry)
-  (let [journal-writer (open-journal db-atom)
+  (post-journal-entry! db-atom journal-entry)
+  (let [journal-writer (open-journal! db-atom)
         journal-entry-edn (pr-str journal-entry)]
     (doto journal-writer (.write (str journal-entry-edn "\n")) (.flush))))
 
-(defn load-ledger
+(defn load-ledger!
   [db-atom]
   (swap! db-atom
          (fn [db]
@@ -87,9 +87,23 @@
                  db (assoc db :value value)]
              db))))
 
-(defn load-journal
+(defn load-journal!
   [db-atom journal-filename]
   (println "Loading file" journal-filename)
   (with-open [journal-reader (io/writer journal-filename :encoding "UTF-8")]
     (for [journal-entry (line-seq journal-reader)]
-      (post-journal-entry db-atom journal-entry))))
+      (post-journal-entry! db-atom journal-entry))))
+
+(defn load-db!
+  [db-atom]
+  (load-ledger! db-atom)
+  (let [db @db-atom
+        folder (:folder db)
+        base (:base db)
+        files (files/resolve-files folder "journal" base "edn")
+        ledger-suffix (:suffix db)]
+    (for [e files]
+      (let [journal-suffix (key e)
+            journal-filemap (val e)]
+        (if (< (compare journal-suffix ledger-suffix) 0)
+          (load-journal! db-atom (files/file-str journal-filemap)))))))
